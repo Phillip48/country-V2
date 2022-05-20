654238f4c662ef0c82f2f7c119ec4fa1a0bf092c
Phillip48 <phillippereira02@gmail.com>
2022-05-19 22:46:40 -0400

    Error in rendering data. Need to Fix some data coming back in arrays

--- controllers/search-routes.js
+++ controllers/search-routes.js
@@ -34,7 +34,8 @@ router.get('/:countryTitle', (req, res) => {
                 currencies: testCountryCurrencySlicedParsed.name,
                 country_img: res.data[0].flags.png,
             }
-            console.log(userCountrySearch);
+            // console.log(userCountrySearch);
+            countryFind(userCountrySearch);
         });
 
 
@@ -63,7 +64,7 @@ router.get('/:countryTitle', (req, res) => {
     // async function
     async function countryFind(userCountrySearch) {
         console.log('Looking...')
-
+        // console.log(userCountrySearch);
         try {
             // Trying to find a country by its title in the db
             const data = await Country.findOne({
@@ -73,12 +74,13 @@ router.get('/:countryTitle', (req, res) => {
 
             // If data is returned from the database then it will get the data
             if (data) {
+                console.log('Looking for data:', data);
                 const userInfo = data.get({ plain: true })
                 const countryID = data.id
                 const reviews = userInfo.reviews
                 const userId = req.session.userId
                 console.log('found entry')
-                res.render('homepage', { userCountrySearch, userId, countryID, reviews, loggedIn: req.session.loggedIn });
+                res.render('homepage', { userCountrySearch, });
             } else {
                 // If there is no data of this country in the database then it will take the data from the API and create it to the database
                 console.log('no entry found')
@@ -90,6 +92,7 @@ router.get('/:countryTitle', (req, res) => {
             }
             // If not a valid country or something goes wrong then it will send you to the 404 page
         } catch (err) {
+            console.log('There was an Error:', err);
             res.render('404')
         }
     };
