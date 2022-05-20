3665d7663653aac49833679dd6605cfabc2558bd
Phillip48 <phillippereira02@gmail.com>
2022-05-19 22:40:38 -0400

    Now getting back all data needed from database. Need to render data

--- controllers/search-routes.js
+++ controllers/search-routes.js
@@ -13,7 +13,14 @@ router.get('/:countryTitle', (req, res) => {
     // Backend or server side search
     axios.get(apiUrl)
         .then((res) => {
-            console.log('Response....', res.data[0].currencies);
+            // console.log('Response....', res.data[0].currencies);
+            // console.log(JSON.stringify(res.data[0].currencies));
+            testCountryCurrencyString = JSON.stringify(res.data[0].currencies)
+            testCountryCurrencySliced = testCountryCurrencyString.slice(7, -1);
+            // console.log(testCountryCurrencySliced);
+            testCountryCurrencySlicedParsed = JSON.parse(testCountryCurrencySliced)
+            // console.log(testCountryCurrencySlicedParsed);
+
             // console.log('Response....', res.data[0].flags.png);
             // Returns the data from the api in an object with the info i need with a callback variable 
             let userCountrySearch = {
@@ -24,16 +31,36 @@ router.get('/:countryTitle', (req, res) => {
                 subregion: res.data[0].subregion,
                 languages: res.data[0].languages,
                 borders: res.data[0].borders,
-                currencies: res.data[0].currencies.this.name,
+                currencies: testCountryCurrencySlicedParsed.name,
                 country_img: res.data[0].flags.png,
             }
-
-            // Calls the function to see if this country is already in the database
-            // Passes in the country as a parameter
-            countryFind(userCountrySearch);
+            console.log(userCountrySearch);
         });
 
-        // async function
+
+    // axios.get(apiUrl)
+    //     .then((res) => {
+    //         console.log('Response....', res.data[0].currencies);
+    //         // console.log('Response....', res.data[0].flags.png);
+    //         // Returns the data from the api in an object with the info i need with a callback variable 
+    //         let userCountrySearch = {
+    //             // Matches the columns in the db
+    //             country_title: res.data[0].name.common,
+    //             capital: res.data[0].capital,
+    //             region: res.data[0].region,
+    //             subregion: res.data[0].subregion,
+    //             languages: res.data[0].languages,
+    //             borders: res.data[0].borders,
+    //             currencies: res.data[0].currencies.this.name,
+    //             country_img: res.data[0].flags.png,
+    //         }
+
+    //         // Calls the function to see if this country is already in the database
+    //         // Passes in the country as a parameter
+    //         countryFind(userCountrySearch);
+    //     });
+
+    // async function
     async function countryFind(userCountrySearch) {
         console.log('Looking...')
 
@@ -51,7 +78,7 @@ router.get('/:countryTitle', (req, res) => {
                 const reviews = userInfo.reviews
                 const userId = req.session.userId
                 console.log('found entry')
-                res.render('homepage', { userCountrySearch, userId, countryID, reviews, loggedIn:req.session.loggedIn });
+                res.render('homepage', { userCountrySearch, userId, countryID, reviews, loggedIn: req.session.loggedIn });
             } else {
                 // If there is no data of this country in the database then it will take the data from the API and create it to the database
                 console.log('no entry found')
@@ -59,7 +86,7 @@ router.get('/:countryTitle', (req, res) => {
                 const userId = req.session.userId
                 const countryID = createdMovie.id
                 const reviews = createdMovie.reviews
-                res.render('homepage', { userCountrySearch, userId, countryID, reviews, loggedIn:req.session.loggedIn });
+                res.render('homepage', { userCountrySearch, userId, countryID, reviews, loggedIn: req.session.loggedIn });
             }
             // If not a valid country or something goes wrong then it will send you to the 404 page
         } catch (err) {
