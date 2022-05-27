6b06ec347298045028859a44aa614081095f0a58
Phillip48 <phillippereira02@gmail.com>
2022-05-26 20:53:00 -0400

    Sign up works. Small fixes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -5,8 +5,8 @@ const { User } = require('../../models');
 router.post('/', async (req, res) => {
   try {
     const dbUserData = await User.create({
-      username: req.body.username,
       email: req.body.email,
+      username: req.body.username,
       password: req.body.password,
     });
 
